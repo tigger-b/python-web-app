b4ee7055af4426ec5d0056495198a814e3c53cf2
tigger-b <tigger-b@users.noreply.github.com>
2021-07-07 17:10:34 +1000

    rock/paper/scissors form submission

--- app.py
+++ app.py
@@ -4,4 +4,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return """
+<style>
+h1 {
+  font-family: monospace;
+  color: #33b343;
+}
+</style>
+<h1>Rock Paper Scissors</h1>
+<small>see if you can beat the computer</small>
+
+<form action="/">
+<input type="submit" value="rock" name="weapon">
+<input type="submit" value="paper" name="weapon">
+<input type="submit" value="scissors" name="weapon">
+</form>
+<pre>
+</pre>
+"""
